71becfc589b7e9d6684801a2ed3b46e8782bc65a
iasjem <jemimah.beryl.sai@gmail.com>
2019-04-17 05:24:11 -0700

    Add models for Post and Comment

---
+++ server/model/Comment.js
@@ -0,0 +1,11 @@
+class Post {
+    constructor(postId, id, name, email, body) {
+        this.postId = postId
+        this.id = id 
+        this.name = name
+        this.email = email
+        this.body = body
+    }    
+}
+
+module.exports = Post;
