0216a223f7268ff4bd285e6e2354fdd5b65711ca
iasjem <jemimah.beryl.sai@gmail.com>
2019-04-17 04:50:38 -0700

    Add new routes for user login and modify routes for views

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,8 @@
+const { Err } = require('../model/Error');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => res.render('index.hbs'));
-router.get('*', (req, res) => res.render('404.hbs'));
 
 router.use('/user', require('./user.js'));
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,16 @@
+const { Err } = require('../model/Error');
+const UserController = require('../controller/user.controller');
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', (req, res) => {
-    res.render('index.hbs');
-});
-router.get('/logout', (req, res) => res.render('login.hbs'));
+router.route('/login')
+    .get((req, res) => res.render('login.hbs'))
+    .post(async (req, res) => {
+        const result = await UserController.login(api.findUserById(req.params.email));
+        if (!result) return res.status(400).render('error.hbs', { Error: Err.userNotFound });
+        res.render('index.hbs', { User: result });
+    });
+
+router.post('/logout', (req, res) => res.redirect('/user/login'));
 
 module.exports = router;
