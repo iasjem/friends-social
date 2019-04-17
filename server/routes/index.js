935323ed379df161780dc4b2574544a5a2ad21f8
iasjem <jemimah.beryl.sai@gmail.com>
2019-04-16 18:20:20 -0700

    Setup index routes

---
+++ server/routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => res.render('index.hbs'));
+router.get('*', (req, res) => res.render('404.hbs'));
+
+module.exports = router;
