21076fb7a6dfda98fe5bcb61e73ac7f85b8013e6
iasjem <jemimah.beryl.sai@gmail.com>
2019-04-17 04:49:39 -0700

    Create and modify error view and add model for Error

---
+++ server/model/Error.js
@@ -0,0 +1,14 @@
+const Err = {
+    userNotFound: {
+        title: "400",
+        message: "User does not exist. Please try again!"
+    },
+    pageNotFound: {
+        title: "404",
+        message: "Page not found"
+    }
+};
+
+module.exports = {
+    Err
+}
