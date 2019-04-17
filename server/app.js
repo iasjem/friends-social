7bada313eef1c6dd51235f5f5d15ec763e90ec31
iasjem <jemimah.beryl.sai@gmail.com>
2019-04-17 05:34:39 -0700

    Add body parser feature to app

--- server/app.js
+++ server/app.js
@@ -4,9 +4,11 @@ console.log(`Environment --- ${process.env.NODE_ENV}`);
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static(path.join(__dirname + './../public/')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', hbs);
 app.set('views', path.join(__dirname + '/views'));
