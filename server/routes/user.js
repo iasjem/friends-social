9bad63474da6c53f5020d2d9c946cc7eb6867938
iasjem <jemimah.beryl.sai@gmail.com>
2019-04-16 21:04:05 -0700

    Change redirect to render in routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', (req, res) => res.redirect('index.hbs'));
-router.get('/logout', (req, res) => res.redirect('login.hbs'));
+router.get('/login', (req, res) => {
+    res.render('index.hbs');
+});
+router.get('/logout', (req, res) => res.render('login.hbs'));
 
 module.exports = router;
