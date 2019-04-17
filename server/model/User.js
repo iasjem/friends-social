17e9d4059bb577fbe80cec2fc38e1b3c5ebae566
iasjem <jemimah.beryl.sai@gmail.com>
2019-04-17 04:51:13 -0700

    Add model for User

---
+++ server/model/User.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(id, name, username, email) {
+        this.id = id
+        this.name = name 
+        this.username = username
+        this.email = email
+    }    
+}
+
+module.exports = User;
