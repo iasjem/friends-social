c95023d7de8e8ced6bf22834c6439d639091c5a0
iasjem <jemimah.beryl.sai@gmail.com>
2019-04-19 00:55:37 +0800

    Add more objects to model Error

--- server/model/Error.js
+++ server/model/Error.js
@@ -1,11 +1,23 @@
 const Err = {
     userNotFound: {
-        title: "400",
-        message: "User does not exist. Please try again!"
+        title: "Login Error",
+        message: "User does not exist. Please try again."
+    },
+    postsNotFound: {
+        title: "Empty User Feed",
+        message: "No posts yet for this user."
+    },
+    commentsNotFound: {
+        title: "Empty Comment Section",
+        message: "No comments yet for this post."
+    },
+    feedLoadFailure: {
+        title: "Loading Error",
+        message: "Server cannot load feed. Please check your internet connection."
     },
     pageNotFound: {
         title: "404",
-        message: "Page not found"
+        message: "Page not found. Please comeback later."
     }
 };
 
