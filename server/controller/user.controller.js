a3bab58fa57f29f12590a6f6cb3bb9888c2159a4
iasjem <jemimah.beryl.sai@gmail.com>
2019-04-19 04:10:23 -0700

    Add new methods for User Controller

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,13 +1,38 @@
+const api = require('../api/users');
+const view = require('../view/user');
+const { errorView } = require('../view/error');
+const { Err } = require('../model/Error');
 let { User } = require('../model/User');
 
-const login = async (findCallback) => {
-    const user = await findCallback;
-    if (!user) return
+const load = user => {
     let { id, name, username, email } = user;
     User = { id, name, username, email };
     return User;
 };
 
+const login = async (email) => {
+    const user = await api.findUserByEmail(email);
+    if (!user) return errorView(Err.userNotFound);
+    let User = load(user);
+    return view.userHeroView(User);
+};
+
+const profile = async (userId) => {
+    const user = await api.findUserById(userId);
+    if (!user) return errorView(Err.pageNotFound);
+    let User = load(user);
+    return view.userView(User);
+};
+
+const search = async (username) => {
+    const user = await api.findUserByUsername(username);
+    if (!user) return errorView(Err.userNotFound);
+    let User = load(user);
+    return view.userSearchView(User);
+};
+
 module.exports = {
-    login
+    login,
+    profile,
+    search
 }
