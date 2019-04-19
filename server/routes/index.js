c086f8d320398c53221f99d02cd84d1673134515
iasjem <jemimah.beryl.sai@gmail.com>
2019-04-19 07:09:16 -0700

    Modify routes for index and user

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,20 @@
-const { Err } = require('../model/Error');
+const api = require('../api/users');
+const UserController = require('../controller/user.controller');
+const PostController = require('../controller/post.controller');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.render('index.hbs'));
+router.route('/') 
+    .get((req, res) => res.render('login.hbs'))
+    .post(async (req, res) => {
+        const userViewController = await UserController.login(req.body.email);
+        const user = await api.findUserByEmail(req.body.email);
+        const postViewController = await PostController.userFeed(user.id);
+        res.render('index.hbs', { userViewController, postViewController });
+    });
 
 router.use('/user', require('./user.js'));
+
+router.use('/posts', require('./post.js'));
 
 module.exports = router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,17 +1,15 @@
-const api = require('../api/users');
-const { Err } = require('../model/Error');
 const UserController = require('../controller/user.controller');
+const PostController = require('../controller/post.controller');
 const express = require('express');
 const router = express.Router();
 
-router.route('/login')
-    .get((req, res) => res.render('login.hbs'))
-    .post(async (req, res) => {
-        const result = await UserController.login(api.findUserByEmail(req.body.email));
-        if (!result) return res.status(400).render('error.hbs', { Error: Err.userNotFound });
-        res.render('index.hbs', { User: result });
-    });
+router.get('/:id', async (req, res) => {
+    let userId = req.params.id;
+    const userViewController = await UserController.profile(userId);
+    const postViewController = await PostController.userFeed(userId);
+    res.render('user.hbs', { userViewController, postViewController });
+});
 
-router.post('/logout', (req, res) => res.redirect('/user/login'));
+router.post('/logout', (req, res) => res.redirect('/'));
 
 module.exports = router;
