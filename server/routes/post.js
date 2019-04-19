6f3802b85d026e1d4e13141549cbf61522b4328f
iasjem <jemimah.beryl.sai@gmail.com>
2019-04-19 07:08:41 -0700

    Add routes for posts

---
+++ server/routes/post.js
@@ -0,0 +1,18 @@
+const PostController = require('../controller/post.controller');
+const CommentController = require('../controller/comment.controller');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const postViewController = await PostController.feed();
+    res.render('user.hbs', { postViewController });
+});
+
+router.get('/:id', async (req, res) => {
+    let postId = req.params.id;
+    const postViewController = await PostController.postPreview(postId);
+    const commentsViewController = await CommentController.postAllComments(postId);
+    res.render('post.hbs', { postViewController, commentsViewController })
+});
+
+module.exports = router;
